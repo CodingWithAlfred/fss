a11844adb6cc8f0a966b8ea3eff81b44d910b2b0
Editopia <Editopia@Homecomputer>
2026-02-24 06:15:12 -0500

    init pipeline

--- build.py
+++ build.py
@@ -2,7 +2,7 @@
 Writes index.html into the ./public directory for Firebase Hosting.
 """
 import os, datetime
-HTML = f"<html><body style='font-family:sans-serif;text-align:center;margin-top:20vh;'><h1>Hello World</h1><p>{datetime.datetime.now():%Y-%m-%d %I:%M:%S %p}</p></body></html>"
+HTML = f"<html><body style='font-family:sans-serif;text-align:center;margin-top:20vh;'><h1>Hello World... last build: </h1><p>{datetime.datetime.now():%Y-%m-%d %I:%M:%S %p}</p></body></html>"
 def build() -> None:
     out = "public/index.html"
     with open(out, "w", encoding="utf-8") as f:
